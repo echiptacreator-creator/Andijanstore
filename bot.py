344d527530f8c8ea8785442e67aa6b0bd1d66ac2
Maladesbola <echiptacreator@gmail.com>
2026-06-11 18:28:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -203,6 +203,15 @@ SKU: {sku}
 
     await state.clear()
 
+from models import Base
+from database import engine
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(
+            Base.metadata.create_all
+        )
+
 
 async def main():
 
