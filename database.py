49712151fa082488e5fffad1f6597e96f87895a5
Maladesbola <echiptacreator@gmail.com>
2026-06-11 17:58:32 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,17 @@
+import os
+
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker
 )
 
-import os
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
-    DATABASE_URL,
-    echo=True
+    DATABASE_URL
 )
 
-SessionLocal = async_sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
     engine,
     expire_on_commit=False
 )
