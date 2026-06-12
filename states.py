3984d733e7e794f7f6633f03e80eb000098071e4
Maladesbola <echiptacreator@gmail.com>
2026-06-12 13:24:01 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,10 +1,5 @@
-from aiogram.fsm.state import State
-from aiogram.fsm.state import StatesGroup
+from aiogram.fsm.state import State, StatesGroup
 
-from aiogram.fsm.state import (
-    StatesGroup,
-    State
-)
 
 class ProductCreate(StatesGroup):
     image = State()
@@ -22,29 +17,4 @@ class ProductCreate(StatesGroup):
 
 class ProductSearch(StatesGroup):
     select_product = State()
-    
-from aiogram.fsm.state import State
-from aiogram.fsm.state import StatesGroup
-
-class ProductCreate(StatesGroup):
-    image = State()
-    name = State()
-    purchase_price = State()
-    sale_price = State()
-    sizes = State()      # yangi
-    quantity = State()
-
-
-class ProductSearch(StatesGroup):
-
-    select_product = State()
-
-
-from aiogram.fsm.state import State
-from aiogram.fsm.state import StatesGroup
-
-class ProductSearch(StatesGroup):
-    select_product = State()
     page = State()
-
-
