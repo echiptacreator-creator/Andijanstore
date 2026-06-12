57880e010e962650c6cebe44f808168de9f0e040
Maladesbola <echiptacreator@gmail.com>
2026-06-12 12:47:03 +0500

    Update states.py

--- states.py
+++ states.py
@@ -38,3 +38,14 @@ from aiogram.fsm.state import StatesGroup
 class ProductSearch(StatesGroup):
     select_product = State()
     page = State()
+
+
+from aiogram.fsm.state import State
+from aiogram.fsm.state import StatesGroup
+
+class ProductCreate(StatesGroup):
+    image = State()
+    name = State()
+    purchase_price = State()
+    sale_price = State()
+    quantity = State()
