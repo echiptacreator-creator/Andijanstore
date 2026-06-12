4aeb47f4d96bbc304b4888ccb3bf236fad02bb31
Maladesbola <echiptacreator@gmail.com>
2026-06-12 18:23:28 +0500

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -14,3 +14,24 @@ async def create():
 
 
 asyncio.run(create())
+
+
+from sqlalchemy import text
+
+async def create_tables():
+
+    async with engine.begin() as conn:
+
+        await conn.run_sync(
+            Base.metadata.create_all
+        )
+
+        await conn.execute(text("""
+            ALTER TABLE products
+            ADD COLUMN IF NOT EXISTS size_xxl INTEGER DEFAULT 0
+        """))
+
+        await conn.execute(text("""
+            ALTER TABLE products
+            ADD COLUMN IF NOT EXISTS size_xxxl INTEGER DEFAULT 0
+        """))
