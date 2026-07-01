5d067a01ee77d800e72c657a5550b4644985c3d7
Maladesbola <echiptacreator@gmail.com>
2026-07-02 04:50:50 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,31 +1,56 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
+# ==========================
+# PRODUCT CREATE
+# ==========================
 class ProductCreate(StatesGroup):
     image = State()
     name = State()
     purchase_price = State()
     sale_price = State()
 
+    # Misol:
+    # S=5
+    # M=8
+    # L=10
+    #
+    # yoki
+    #
+    # 26=5
+    # 28=8
+    # 30=10
     sizes = State()
-    
+
     confirm = State()
 
 
+# ==========================
+# PRODUCT SEARCH
+# ==========================
 class ProductSearch(StatesGroup):
     select_product = State()
     page = State()
 
-class ExpenseCreate(StatesGroup):
 
+# ==========================
+# EXPENSE
+# ==========================
+class ExpenseCreate(StatesGroup):
     title = State()
     amount = State()
     payment_type = State()
 
 
+# ==========================
+# SALE
+# ==========================
 class SaleCreate(StatesGroup):
-
     sku = State()
+
+    # Dinamik razmer (S, 26, FREE...)
     size = State()
+
     quantity = State()
 
     payment = State()
